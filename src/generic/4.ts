1da7d2c306f7a598e1744f3f1e72c37416e95d86
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2023-11-25 21:26:22 +0200

    done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,17 +1,19 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
+interface ComponentProps{
+  title: string
+}
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component <ComponentProps>{
   pageInfo () {
     console.log(this.props.title);
   }
 }
 
-export {};
+export { };
